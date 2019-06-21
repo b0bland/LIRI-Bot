94cc13bcb4b5b5b4bee5d8899b264791009a0887
b0bland <alfredychan96@gmail.com>
2019-06-21 00:28:35 -0700

    Finished concert-this

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require('axios');
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 
 var task = process.argv[2];
 var term = process.argv.slice(3).join(" ");
@@ -10,7 +10,16 @@ var term = process.argv.slice(3).join(" ");
 if (task === "concert-this") {
     var bandURL = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp";
     axios.get(bandURL).then(function(response) {
-        console.log(response);
-
+        var jsonData = response.data;
+        console.log(jsonData.length);
+        for (n=0;n<jsonData.length;n++) {
+            var eventData = [
+                "Venue: " + jsonData[n].venue.name,
+                "Location: " + jsonData[n].venue.city + ", " + jsonData[n].venue.country,
+                "Date: " + jsonData[n].datetime.split("T").join(" "),
+                "\n-------------------\n"
+            ].join("\n");
+            console.log(eventData);
+        }
     })
 }
