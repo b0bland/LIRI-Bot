27e1267b1d3184e5b6d3f75d0071de6cd25e1c38
b0bland <alfredychan96@gmail.com>
2019-06-21 00:53:20 -0700

    Added spotify.search for spotify-this-song

--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require('axios');
+var Spotify = require('node-spotify-api');
 
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 var task = process.argv[2];
 var term = process.argv.slice(3).join(" ");
@@ -11,7 +12,6 @@ if (task === "concert-this") {
     var bandURL = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp";
     axios.get(bandURL).then(function(response) {
         var jsonData = response.data;
-        console.log(jsonData.length);
         for (n=0;n<jsonData.length;n++) {
             var eventData = [
                 "Venue: " + jsonData[n].venue.name,
@@ -22,4 +22,16 @@ if (task === "concert-this") {
             console.log(eventData);
         }
     })
+}
+else if (task === "spotify-this-song") {
+    spotify.search({ type: 'track', query: term }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+    //   data.tracks.items[n].artists[0].name
+        // data.tracks.items[n].name
+        // data.tracks.items[n].preview_url
+        // data.tracks.items[n].album.name
+      });
 }
